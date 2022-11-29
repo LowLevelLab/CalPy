908ac1550ad9f8a627914d67f7c1be74f2b8e02e
Varad_comrad <fabricio_asfora_002@hotmail.com>
2022-11-29 14:10:56 -0300

    biblioteca cálculo numérico 1.10.6.1

--- test.py
+++ test.py
@@ -1,22 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from polynomials import Polynomial
 from differential_equations import ODE
-from functions import Function
-from arrays import Vector
-from lin_regression import *
+
+y1 = lambda x,y,z : x+y-z
+y2 = lambda x,y,z : x-y-2*z
+y = ODE([y1,y2],[0,1])
+data = y.euler2([1,2])
 
 
-a = Function(lambda x, y, z: y+z)
-b = Function(lambda x, y, z: z+y)
-# c = Regression([1,2,3,4], [2,np.exp2(2),np.exp2(3),np.exp2(4)])
-# exp = c('exp')
-# print(exp)
 
-
-sol = ODE([a,b], [0,10])
-table = sol.euler([1,1],df=False)
-e = sol.error([lambda x: np.exp(2*x), lambda x: np.exp(2*x)],table, type='percent')
-print(e)
 
